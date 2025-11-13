480c46516f4d2a20650a866eeb0d50aaa56969cd
Daniel <danielmarchenajimenez2002@gmail.com>
2025-11-13 13:03:10 +0100

    FileConnector generic

--- dsl-framework/src/main/java/iia/dsl/framework/connectors/FileConnector.java
+++ dsl-framework/src/main/java/iia/dsl/framework/connectors/FileConnector.java
@@ -6,27 +6,65 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class FileConnector extends Connector {
     private final String filePath;
+    private final int type;
+    private final File file;
+
+    private static final int READ = 0;
+    private static final int WRITE = 1;
     
+    public FileConnector(String id, String filePath, Document xml) {
+        super(id);
+        this.filePath = filePath;
+        this.type = WRITE;
+        this.file = new File(filePath);
+    }
+
     public FileConnector(String id, String filePath) {
         super(id);
         this.filePath = filePath;
+        this.type = READ;
+        this.file = new File(filePath);
     }
     
     @Override
     public Document call(Document input) {
-        // Lee XML desde archivo y lo convierte a Document
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            return builder.parse(new File(filePath));
-        } catch (IOException | ParserConfigurationException | SAXException e) {
-            throw new RuntimeException("Error reading file: " + filePath, e);
+        switch (type) {
+            case READ -> {
+                try {
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(file);
+                    doc.getDocumentElement().normalize();
+                    return doc;
+                } catch (ParserConfigurationException | SAXException | IOException e) {
+                    throw new RuntimeException("Error reading file: " + filePath, e);
+                }
+            }
+            case WRITE -> {
+                // Write document to file
+                try {
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+                    DOMSource source = new DOMSource(input);
+                    StreamResult result = new StreamResult(file);
+                    transformer.transform(source, result);
+                    return null;
+                } catch (TransformerException e) {
+                    throw new RuntimeException("Error writing to file: " + filePath, e);
+                }
+            }
+            default -> throw new IllegalArgumentException("Invalid type: " + type);
         }
     }
 }
